4590739a660b96da9420f5c73cb2f8de0e2ce543
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2022-01-15 14:56:41 +0100

    Simple page setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  // { path: 'first-component', component: FirstComponent },
+  // { path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
   declarations: [
