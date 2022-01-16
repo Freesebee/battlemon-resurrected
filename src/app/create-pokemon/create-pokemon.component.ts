5b03a09723d8085ec9ffd522ebc8a4359e2dc75c
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-01-17 00:13:12 +0100

    Create and edit works

--- src/app/create-pokemon/create-pokemon.component.ts
+++ src/app/create-pokemon/create-pokemon.component.ts
@@ -6,56 +6,82 @@ import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-pokemon',
   templateUrl: './create-pokemon.component.html',
-  styleUrls: ['./create-pokemon.component.scss']
+  styleUrls: ['./create-pokemon.component.scss'],
 })
 export class CreatePokemonComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor(private formBuilder: FormBuilder) {
 
-  }
   @Input() battlemon!: IBattlemon;
   @Output() newBattlemon = new EventEmitter<IBattlemon>();
 
   createForm = this.formBuilder.group({
-    id: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(10)]],
-    name: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(10)]],
-    type: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(10)]],
-    dmg: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(10)]],
-    hp: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(10)]],
-    crit_chance: ['',[Validators.required,Validators.minLength(1),Validators.maxLength(3),Validators.min(0),Validators.max(100)]],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(1), Validators.maxLength(10)],
+    ],
+    type: [
+      '',
+      [Validators.required, Validators.minLength(1), Validators.maxLength(10)],
+    ],
+    dmg: [
+      '',
+      [Validators.required, Validators.minLength(1), Validators.maxLength(10)],
+    ],
+    hp: [
+      '',
+      [Validators.required, Validators.minLength(1), Validators.maxLength(10)],
+    ],
+    crit_chance: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(3),
+        Validators.min(0),
+        Validators.max(100),
+      ],
+    ],
   });
 
   onSubmit(value: IBattlemon): void {
-    this.battlemon.id = this.createForm.get('id')?.value;
-    this.battlemon.name = this.createForm.get('name')?.value
-    this.battlemon.dmg = this.createForm.get('dmg')?.value
-    this.battlemon.crit_chance = this.createForm.get('crit_chance')?.value
-    this.battlemon.hp = this.createForm.get('hp')?.value
-    this.battlemon.type = this.createForm.get('type')?.value
-
+    if (this.battlemon == undefined) {
+      this.battlemon = {
+        id:0,
+        name: this.createForm.get('name')?.value,
+        dmg: this.createForm.get('dmg')?.value,
+        crit_chance:this.createForm.get('name')?.value,
+        hp: this.createForm.get('hp')?.value,
+        type: this.createForm.get('type')?.value
+      };
+    } else {
+      this.battlemon.name = this.createForm.get('name')?.value;
+      this.battlemon.dmg = this.createForm.get('dmg')?.value;
+      this.battlemon.crit_chance = this.createForm.get('crit_chance')?.value;
+      this.battlemon.hp = this.createForm.get('hp')?.value;
+      this.battlemon.type = this.createForm.get('type')?.value;
+    }
     this.newBattlemon.emit(this.battlemon);
     this.createForm.reset();
-
   }
 
-  ngOnInit(): void {
-  }
-  get id(){
+  ngOnInit(): void {}
+  get id() {
     return this.createForm.get('id');
   }
-  get name(){
+  get name() {
     return this.createForm.get('name');
   }
-  get type(){
+  get type() {
     return this.createForm.get('type');
   }
-  get dmg(){
+  get dmg() {
     return this.createForm.get('dmg');
   }
-  get hp(){
+  get hp() {
     return this.createForm.get('hp');
   }
-  get crit_chance(){
+  get crit_chance() {
     return this.createForm.get('crit_chance');
   }
 }
--- src/app/pokemon-table/pokemon-table.component.ts
+++ src/app/pokemon-table/pokemon-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { CreatePokemonComponent } from '../create-pokemon/create-pokemon.component';
 import IBattlemon from '../interfaces/IBattlemon';
 import { BattlemonService } from 'src/app/services/battlemon.service';
@@ -7,34 +7,69 @@ import { BattlemonService } from 'src/app/services/battlemon.service';
 @Component({
   selector: 'app-pokemon-table',
   templateUrl: './pokemon-table.component.html',
-  styleUrls: ['./pokemon-table.component.scss']
+  styleUrls: ['./pokemon-table.component.scss'],
 })
 export class PokemonTableComponent implements OnInit {
-
-  constructor(public dialog: MatDialog,private _battlemonService: BattlemonService) { }
+  constructor(
+    public dialog: MatDialog,
+    private _battlemonService: BattlemonService
+  ) {}
 
   @Input() battlemons!: IBattlemon[];
 
-
   openCreateDialog(): void {
-    const dialogRef = this.dialog.open(CreatePokemonComponent,{
-      width: '640px',disableClose: true
+    const dialogRef = this.dialog.open(CreatePokemonComponent, {
+      width: '640px',
+      disableClose: true,
     });
   }
   ngOnInit(): void {
-    this.onGetAll()
-
+    this.onGetAll();
   }
   onGetAll() {
     this._battlemonService.GetAllBattlemons().subscribe({
       next: (result: any) => {
         console.log(result);
-        this.battlemons = result
+        this.battlemons = result;
       },
       error: (error: any) => {
         console.error(error);
       },
     });
   }
-
+  createNewBattlemon(createdBattlemon: IBattlemon) {
+    this._battlemonService.CreateBattlemon(createdBattlemon).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        this.battlemons.push(result);
+      },
+      error: (error: any) => {
+        console.error(error);
+      },
+    });
+  }
+  editBattlemon(battlemonToEdit: IBattlemon) {
+    this._battlemonService.UpdateBattlemon(battlemonToEdit).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        var index =  this.battlemons.findIndex(x => x.id==result.id);
+        this.battlemons[index] = result;
+      },
+      error: (error: any) => {
+        console.error(error);
+      },
+    });
+  }
+  deleteBattlemon(battlemonToDelete: IBattlemon) {
+    this._battlemonService.DeleteBattlemon(battlemonToDelete.id).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        var index =  this.battlemons.findIndex(x => x.id==result.id);
+        this.battlemons.splice(index,1);
+      },
+      error: (error: any) => {
+        console.error(error);
+      },
+    });
+  }
 }
