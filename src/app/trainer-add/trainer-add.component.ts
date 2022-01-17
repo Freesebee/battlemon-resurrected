4ebdd9c8c2d12eb980a9b2a9d9a616022188f621
mateuszkaranowski <mateuszkaranowski@gmail.com>
2022-01-17 21:13:31 +0100

    Dodanie przycisku do ekranu głownego, dodawanie trenera

--- src/app/trainer-add/trainer-add.component.ts
+++ src/app/trainer-add/trainer-add.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
+import ITrainer from "../interfaces/ITrainer";
+import {Validator} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FormBuilder} from "@angular/forms";
+
 
 @Component({
   selector: 'app-trainer-add',
@@ -6,10 +11,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainer-add.component.scss']
 })
 export class TrainerAddComponent implements OnInit {
+  @Input() trainer!: ITrainer;
+  @Output() trainerAdded = new EventEmitter<ITrainer>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public dialog: MatDialogRef<TrainerAddComponent>,
+    private formTrainer: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public info: any
+  ) {
   }
+
+  newForm = this.formTrainer.group({
+    name: [''],
+    gender: [''],
+    taunt_text: [''],
+  });
+
+  ngOnInit(): void
+  { }
+
+  get id()
+  {
+    return this.newForm.get('id');
+  }
+
+  get name()
+  {
+    return this.newForm.get('name');
+  }
+
+  get gender()
+  {
+    return this.newForm.get('gender');
+  }
+
+  /*get matches_won() {
+    return this.newForm.get('matches_won');
+  }
+  get matches_lost() {
+    return this.newForm.get('matches_lost');
+  }*/
+  get taunt_text()
+  {
+    return this.newForm.get('taunt_text');
+  }
+
+  exit()
+  {
+    this.dialog.close()
+  }
+
+  save()
+  {
+    this.dialog.close(this.newForm.value)
+  }
+
 
 }
